afed60edbbb28393324944d1fcc205df1d19b503
Almansur342 <abdullahalmansur2002@gmail.com>
2024-05-18 19:30:33 +0600

    problem 1 solved

--- Assignment/problem1.cpp
+++ Assignment/problem1.cpp
@@ -3,28 +3,30 @@
 using namespace std;
 
 signed main() {
-    // Input values
+   
     double lambda, theta, d;
     cout << "Enter the wavelength (in nm): "<<endl;
     cin >> lambda;
     cout << "Enter the angle (in degrees): "<<endl;
     cin >> theta;
-    cout << "Enter the distance between the slits (in meters): "<<endl;
+    cout << "Enter the distance between the slits (in micrometers): "<<endl;
     cin >> d;
 
-    // Check if wavelength is within the valid range
+
     if (lambda < 380 or lambda > 750) {
         cout << "Out of the range. Please enter a valid number." << endl;
         return 0;
     }
 
-    // Convert angle from degrees to radians
+
     double theta_rad = theta * M_PI / 180.0;
 
-    // Calculate order of maxima
-    double m = d * sin(theta_rad) / lambda;
+   
+    lambda = lambda * 1e-9; 
+    d = d * 1e-6; 
 
-    // Output the result
+    int m = d * sin(theta_rad) / lambda;
+
     cout << m << "-th order maxima" << endl;
 
     return 0;
